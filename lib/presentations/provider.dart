ff862f86f7786bc3f59b8aa66187f3a24f3e256a
本川　晴彦 <harx6009@icloud.com>
2025-04-05 10:25:52 +0900

    BackgroundColorの状態管理を追加し、色を変更する機能を実装

---
+++ lib/presentations/provider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'provider.freezed.dart';
+part 'provider.g.dart';
+
+@freezed
+class BackgroundColorState with _$BackgroundColorState {
+  const factory BackgroundColorState({
+    @Default(Colors.white) Color backgroundColor,
+  }) = _BackgroundColorState;
+}
+
+@riverpod
+class BackgroundColor extends _$BackgroundColor {
+  @override
+  BackgroundColorState build() {
+    return const BackgroundColorState();
+  }
+
+  Future<void> changeColor() async {
+    final color = switch (state.backgroundColor) {
+      Colors.white => Colors.blue,
+      Colors.blue => Colors.green,
+      Colors.green => Colors.red,
+      Colors.red => Colors.white,
+      _ => Colors.white,
+    };
+
+    // 10秒待つ
+    await Future<void>.delayed(const Duration(seconds: 10));
+
+    state = state.copyWith(backgroundColor: color);
+  }
+}
